ab0f19b3ae9b2b022ee586295cc94999b60014d1
wincax@hotmail.com <huangzhiwen@lfun.cn>
2025-06-16 14:20:38 +0800

    Updates

--- assets/index-5jIG5NiV.js
+++ assets/index-5jIG5NiV.js
@@ -75,9 +75,9 @@ root.render(
       </head>
       <body>
         <div id="root">${i}</div>
-        <script src="https://unpkg.com/react@18/umd/react.development.js"><\/script>
-        <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"><\/script>
-        <script src="https://unpkg.com/@babel/standalone/babel.min.js"><\/script>
+        <script src="/react-live-editor-demo/react.development.js"><\/script>
+        <script src="/react-live-editor-demo/react-dom.development.js"><\/script>
+        <script src="/react-live-editor-demo/standalone/babel.min.js"><\/script>
         <script type="text/babel">
           ${z}
         <\/script>
